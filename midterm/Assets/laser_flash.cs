8ebedd7ce515a31854a7e42cbe17fd79c8cd14e4
esharo02 <eyal.sharon@tufts.edu>
2023-03-05 12:26:07 -0500

    Update laser_flash.cs

--- midterm/Assets/laser_flash.cs
+++ midterm/Assets/laser_flash.cs
@@ -1,50 +1,17 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-
-
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-// public class laser_flash : MonoBehaviour
-
-
-// void Start()
-// {
-
-// }
-// void Update{
-
-// }
-//  {
-    
-//      void Start()
+// public GameObject theSprite; //laser
+ 
+//  void Start()
 //  {
 //      StartCoroutine(StartBlinking()); //Doesn't need to be in Start() but use t$$anonymous$$s line wherever you need it.
 //  }
-//    IEnumerator StartBlinking()
-//  {
-//      yield return new WaitForSeconds(1); //However many seconds you want
-//      laser_flash.GetComponent<final_lazer>().enabled = !laser_flash.GetComponent<final_lazer>().enabled; //T$$anonymous$$s toggles it
-//      StartCoroutine(StartBlinking());
-//  }
-
-//      void Update()
-//      {
-         
-
-            
-//      }
-//  }
-
-// public class final_lazer : MonoBehaviour {
-
-
  
 //  IEnumerator StartBlinking()
 //  {
 //      yield return new WaitForSeconds(1); //However many seconds you want
-//      final_lazer.GetComponent<SpriteRenderer>().enabled = !final_lazer.GetComponent<SpriteRenderer>().enabled; //T$$anonymous$$s toggles it
+//      theSprite.GetComponent<SpriteRenderer>().enabled = !theSprite.GetComponent<SpriteRenderer>().enabled; //T$$anonymous$$s toggles it
 //      StartCoroutine(StartBlinking());
 //  }
-
-// }

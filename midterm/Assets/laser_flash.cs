24380299f3fc510e4938578b0081ab5f9a4dd91f
esharo02 <eyal.sharon@tufts.edu>
2023-03-05 12:13:41 -0500

    working on lasers (start)
    Only messing around with the laser script

---
+++ midterm/Assets/laser_flash.cs
@@ -0,0 +1,50 @@
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// public class laser_flash : MonoBehaviour
+
+
+// void Start()
+// {
+
+// }
+// void Update{
+
+// }
+//  {
+    
+//      void Start()
+//  {
+//      StartCoroutine(StartBlinking()); //Doesn't need to be in Start() but use t$$anonymous$$s line wherever you need it.
+//  }
+//    IEnumerator StartBlinking()
+//  {
+//      yield return new WaitForSeconds(1); //However many seconds you want
+//      laser_flash.GetComponent<final_lazer>().enabled = !laser_flash.GetComponent<final_lazer>().enabled; //T$$anonymous$$s toggles it
+//      StartCoroutine(StartBlinking());
+//  }
+
+//      void Update()
+//      {
+         
+
+            
+//      }
+//  }
+
+// public class final_lazer : MonoBehaviour {
+
+
+ 
+//  IEnumerator StartBlinking()
+//  {
+//      yield return new WaitForSeconds(1); //However many seconds you want
+//      final_lazer.GetComponent<SpriteRenderer>().enabled = !final_lazer.GetComponent<SpriteRenderer>().enabled; //T$$anonymous$$s toggles it
+//      StartCoroutine(StartBlinking());
+//  }
+
+// }
